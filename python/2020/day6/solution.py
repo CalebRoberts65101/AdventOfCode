7e3aef47831d7fb6ea18a3f570690d1f10ae5dc0
Caleb Roberts <calebroberts65101@gmail.com>
2021-11-12 14:55:43 -0600

    Part 1 solution

---
+++ python/2020/day6/solution.py
@@ -0,0 +1,23 @@
+
+with open('python\\2020\day6\data.txt') as f:
+    lines = f.read()
+
+chuncks = lines.split('\n\n')
+
+# part 1
+total_count = 0
+for chunck in chuncks:
+    group_count = 0
+    found_letters = dict()
+    people = chunck.split('\n')
+    for person in people:
+        for char in person:
+            if char not in found_letters:
+                group_count += 1
+                total_count += 1
+                found_letters[char] = 1
+    print("group count: ",group_count)
+
+print("total count:", total_count)
+
+
