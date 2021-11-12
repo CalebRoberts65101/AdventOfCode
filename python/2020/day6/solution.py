c7d7ab3f0efc1a6c8622ef6086b586ad451bd368
Caleb Roberts <calebroberts65101@gmail.com>
2021-11-12 14:58:17 -0600

    Solution to part 2

--- python/2020/day6/solution.py
+++ python/2020/day6/solution.py
@@ -16,8 +16,28 @@ for chunck in chuncks:
                 group_count += 1
                 total_count += 1
                 found_letters[char] = 1
-    print("group count: ",group_count)
+    # print("group count: ",group_count)
 
-print("total count:", total_count)
+# print("total count:", total_count)
+
+#part 2
+total_count = 0
+for chunck in chuncks:
+    group_count = 0
+    found_letters = dict()
+    people = chunck.split('\n')
+    for person in people:
+        for char in person:
+            if char not in found_letters:
+                found_letters[char] = 1
+            else:
+                found_letters[char] += 1
+    for key, value in found_letters.items():
+        if value == len(people):
+            group_count+=1
+            total_count+=1
+    print(group_count)
+
+print('total count:', total_count)
 
 
